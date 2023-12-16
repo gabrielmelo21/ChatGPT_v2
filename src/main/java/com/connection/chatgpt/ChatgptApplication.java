92ea8a7a6b44c716dc86b30f58a656ce3f839f6c
Gabriel de Melo <89091392+gabrielmelo21@users.noreply.github.com>
2023-12-16 19:22:17 -0300

    main mais simples

--- src/main/java/com/connection/chatgpt/ChatgptApplication.java
+++ src/main/java/com/connection/chatgpt/ChatgptApplication.java
@@ -12,16 +12,6 @@ import java.util.Collections;
 public class ChatgptApplication {
 
 	public static void main(String[] args) {
-		// Use a porta fornecida pelo ambiente do Heroku, ou 8080 se não estiver definida
-		String port = System.getenv("PORT");
-		if (port == null || port.isEmpty()) {
-			port = "8080";
-		}
-
-		// Configuração da porta e inicialização do aplicativo
-		SpringApplication app = new SpringApplication(ChatgptApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", port));
-		app.run(args);
+		SpringApplication.run(ChatgptApplication.class, args);
 	}
 }
-
